e5409be79ad9460114f3b5d5a10feb327790c780
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2024-08-03 18:03:53 +0500

    fix

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,17 +8,15 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '../models/user.model';
-import { JwtService } from '@nestjs/jwt';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { LoginUserDto } from './user.dto';
-import { Request } from 'express';
+import { CreateUserDto, LoginUserDto } from './user.dto';
+import { JwtAuthGuard } from 'src/jwt/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('register')
-  async register(@Body() userDto: any): Promise<User> {
+  async register(@Body() userDto: CreateUserDto): Promise<User> {
     return await this.userService.register(userDto);
   }
 
